1fabd562d0229126ed5a6be86795fb66cd949d65
Teck72 <124355169+Teck72@users.noreply.github.com>
2023-05-12 10:24:21 +0200

    Add files via upload

---
+++ Cours.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("ESSAI")
+st.success("Gagné")
+st.error("Perdu")
+st.info("Info")
+
+if st.button('Envoyer'):
+    st.balloons()
+    
+st.warning('This is a warning', icon="⚠️")
+
+st.snow()
